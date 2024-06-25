271315377c5c058f731aa9e7e27408f10861693d
Caio Martins do Nascimento <martinscaio29@gmail.com>
2024-06-24 21:40:52 -0300

    feature: br utils cep com a api do via cep

---
+++ cmd/cep.go
@@ -0,0 +1,60 @@
+package brutils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
+)
+
+// DadosCEP representa a estrutura dos dados retornados pela API do ViaCEP
+type DadosCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd 		string `json:"ddd"`
+	Siafi		string `json:"siafi"`
+}
+
+// ValidaCEP verifica se um CEP está no formato válido
+func ValidaCEP(cep string) bool {
+	// Expressão regular para validar o formato de CEP (opcionalmente com hífen)
+	cepRegex := regexp.MustCompile(`^\d{5}-?\d{3}$`)
+	return cepRegex.MatchString(cep)
+}
+
+// ConsultaCEP realiza uma consulta ao ViaCEP e retorna os dados do CEP
+func ConsultaCEP(cep string) (*DadosCEP, error) {
+	// Verifica se o CEP está no formato válido
+	if !ValidaCEP(cep) {
+		return nil, fmt.Errorf("Formato de CEP inválido")
+	}
+
+	url := fmt.Sprintf("https://viacep.com.br/ws/%s/json/", cep)
+
+	// Realiza a requisição GET para a API do ViaCEP
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	// Verifica o status da resposta
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Erro ao consultar o ViaCEP. Status: %d", resp.StatusCode)
+	}
+
+	// Decodifica a resposta JSON para a struct DadosCEP
+	var dados DadosCEP
+	err = json.NewDecoder(resp.Body).Decode(&dados)
+	if err != nil {
+		return nil, err
+	}
+
+	return &dados, nil
+}
--- main.go
+++ main.go
@@ -13,4 +13,28 @@ func main() {
 	cnpj := "12.345.678/0001-95"
 	fmt.Printf("CNPJ %s é válido? %v\n", cnpj, brutils.ValidaCNPJ(cnpj))
 	fmt.Printf("CNPJ formatado: %s\n", brutils.FormataCNPJ(cnpj))
+
+	cep := "01001-000" // CEP de exemplo
+	if !brutils.ValidaCEP(cep) {
+		fmt.Println("Formato de CEP inválido.")
+		return
+	}
+
+	dadosCEP, err := brutils.ConsultaCEP(cep)
+	if err != nil {
+		fmt.Println("Erro ao consultar o CEP:", err)
+		return
+	}
+
+	// Exibe os dados do CEP consultado
+	fmt.Println("CEP:", dadosCEP.Cep)
+	fmt.Println("Logradouro:", dadosCEP.Logradouro)
+	fmt.Println("Complemento:", dadosCEP.Complemento)
+	fmt.Println("Bairro:", dadosCEP.Bairro)
+	fmt.Println("Localidade:", dadosCEP.Localidade)
+	fmt.Println("UF:", dadosCEP.Uf)
+	fmt.Println("IBGE:", dadosCEP.Ibge)
+	fmt.Println("GIA:", dadosCEP.Gia)
+	fmt.Println("IBGE:", dadosCEP.Ddd)
+	fmt.Println("GIA:", dadosCEP.Siafi)
 }
