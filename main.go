f15209ad1821ddfc5cc4768a06f15ccdc31e5de2
Kauan Pacheco <118062117+5porcento@users.noreply.github.com>
2024-06-25 01:20:45 -0300

    Update main.go
    Teste

--- main.go
+++ main.go
@@ -6,35 +6,10 @@ import (
 )
 
 func main() {
-	cpf := "123.456.789-09"
-	fmt.Printf("CPF %s é válido? %v\n", cpf, brutils.ValidaCPF(cpf))
-	fmt.Printf("CPF formatado: %s\n", brutils.FormataCPF(cpf))
-
-	cnpj := "12.345.678/0001-95"
-	fmt.Printf("CNPJ %s é válido? %v\n", cnpj, brutils.ValidaCNPJ(cnpj))
-	fmt.Printf("CNPJ formatado: %s\n", brutils.FormataCNPJ(cnpj))
-
-	cep := "01001-000" // CEP de exemplo
-	if !brutils.ValidaCEP(cep) {
-		fmt.Println("Formato de CEP inválido.")
-		return
-	}
-
-	dadosCEP, err := brutils.ConsultaCEP(cep)
-	if err != nil {
-		fmt.Println("Erro ao consultar o CEP:", err)
-		return
-	}
-
-	// Exibe os dados do CEP consultado
-	fmt.Println("CEP:", dadosCEP.Cep)
-	fmt.Println("Logradouro:", dadosCEP.Logradouro)
-	fmt.Println("Complemento:", dadosCEP.Complemento)
-	fmt.Println("Bairro:", dadosCEP.Bairro)
-	fmt.Println("Localidade:", dadosCEP.Localidade)
-	fmt.Println("UF:", dadosCEP.Uf)
-	fmt.Println("IBGE:", dadosCEP.Ibge)
-	fmt.Println("GIA:", dadosCEP.Gia)
-	fmt.Println("IBGE:", dadosCEP.Ddd)
-	fmt.Println("GIA:", dadosCEP.Siafi)
+	numero := "(11)912345678"
+	numero2 := "11912345678"
+	numero3 := "1191234567"
+	fmt.Printf("numero %s é válido? %v\n", numero, brutils.ValidaNumeroMovel(numero))
+	fmt.Printf("numero2 %s é válido? %v\n", numero2, brutils.ValidaNumeroMovel(numero2))
+	fmt.Printf("numero3 %s é válido? %v\n", numero3, brutils.ValidaNumeroMovel(numero3))
 }
