ed45f9ea3500148e3123ac0ab719f021ff057f91
Kauan Pacheco <118062117+5porcento@users.noreply.github.com>
2024-06-26 17:24:32 -0300

    Update main.go
    Testes

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/CaioMartinss/br-utils-go/cmd"
+
+	brutils "github.com/CaioMartinss/br-utils-go/cmd"
 )
 
 func main() {
-	numero := "(11)912345678"
-	numero2 := "11912345678"
-	numero3 := "1191234567"
-	fmt.Printf("numero %s é válido? %v\n", numero, brutils.ValidaNumeroMovel(numero))
-	fmt.Printf("numero2 %s é válido? %v\n", numero2, brutils.ValidaNumeroMovel(numero2))
-	fmt.Printf("numero3 %s é válido? %v\n", numero3, brutils.ValidaNumeroMovel(numero3))
+	numero := "11111111213"
+	fmt.Println(brutils.ValidaNumeroMovel(numero))
+	fmt.Println(brutils.FormataNumeroMovel(numero))
+
 }
